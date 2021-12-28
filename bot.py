fd89aeb14460a4fae32999cf3aa342861dadd0e6
Nadia Nadir <60047689+NadiaNadir@users.noreply.github.com>
2021-12-28 14:48:30 +0500

    Update bot.py
    code update - 
    Now calling Binanace Data.

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
-import websocket, json, pprint
+import websocket, json, pprint, talib, numpy
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+RSI_PRIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+TRADE_SYMBOL = 'ETHUSD'
+TRADE_QUANTITY = 0.005
 
 closes = []
+in_position = False
 
 def on_open(ws):
     print('Opened Connection')
@@ -27,6 +33,29 @@ def on_message(ws, message):
         close.append(float(close))
         print("Closes")
         print(closes)
-                              
+        
+        if len(closes) > RSI_PERIOD:
+            np_closes = numpy.array(closes)
+            rsi = talib.RSI(np_closes, RSI_PERIOD)
+            print("All RSIs Calculated so far")
+            print(rsi)
+            last_rsi = rsi[-1]
+            print("The Current  RSI is {}".format(last_rsi))
+            
+            if last_rsi > RSI_OVERBOUGHT:
+                if in_position:
+                    print("OVERBOUGHT! Sell! Sell! Sell!!!")
+                    # Binance SELL-ORDER Logic here 
+                else: 
+                    print("It is OVER BOUGHT, but We don't own any. Nothing to do.")
+            
+            if last_rsi < RSI_OVERSOLD:
+                if in_position:
+                    print("It is OVERSOLD, but you already own it. Nothing to do")
+                else:
+                    print("OVERSOLD! Buy! Buy! Buy!!!")
+                    # Binance BUY-ORDER Logic here 
+                
+            
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
